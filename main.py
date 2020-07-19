0b14cc1bc656462c6a7a8aa645351e2541d7a4a2
Dillon Estrada <dillon.estrada55@gmail.com>
2020-07-19 16:57:34 -0500

    Add ability to separate items with links in them

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from keys import keys
+import todoist
+import re
 
+todoist_api = todoist.TodoistAPI(keys["todoist"])
+todoist_api.sync()
+
+wishlists = []
+regex = r'\[.*?\]\(.*?\)'
+for project in todoist_api.state['projects']:
+    if str(project['parent_id']) == "1799452428":
+        wishlists.append(str(project["id"]))
+
+for list in wishlists:
+    for item in todoist_api.state['items']:
+        if str(item['project_id']) == list:
+            content = item['content']
+            if re.search(regex, content) != None:
+                print("Matched: " + content)
+            else: 
+                print(content)
